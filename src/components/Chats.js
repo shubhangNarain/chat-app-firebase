ec9d9cc8274c78201667211e5b49014d6559508b
Shubhang Narain <snvishere@gmail.com>
2022-07-13 00:34:02 +0530

    More progress uploaded

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
+import { ChatEngine} from 'react-chat-engine';
+import { auth } from '../firebase';
 
-export default function Chats() {
-  return (
-    <div>Chats work</div>
-  )
+import { useAuth } from '../contexts/AuthContext';
+
+const Chats = ()  => {
+    const history = useHistory();
+    const { user } = useAuth();
+
+    console.log(user);
+
+    const handleLogout = async () => {
+        await auth.signOut();
+        history.push('/login');
+    }
+
+    return (
+        <div className="chats-page">
+            <div className='nav-bar'>
+                <div className='logo-tab'>
+                    snvChat
+                </div>
+                <div onClick={handleLogout} className='logout-tab'>
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine
+                height="calc(100vh - 66px)"
+                projectId="f0c14315-6267-47dc-9af6-34ddaafa569d"
+                userName="."
+                userSecret="."
+            />
+        </div>
+    );
 }
+
+export default Chats;
